3c1f35d59d5da9953ca4ab6a0debeaea86a8a2df
jsunh98 <jsunh98@gmail.com>
2023-04-26 00:08:09 -0400

    added chat data functions in chatcomplete.js and renamed html files

--- chatcomplete.js
+++ chatcomplete.js
@@ -1,21 +1,22 @@
 const API_KEY = "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"
 
-async function fetchData() {
-    const response = await fetch("https://api.openai.com/v1/completions", {
+async function fetchChatData() {
+    const response = await fetch("https://api.openai.com/v1/chat/completions", {
         method: "POST",
         headers: {
             Authorization: `Bearer ${API_KEY}`,
             "Content-Type": "application/json"
         },
         body: JSON.stringify({
-            model: "text-davinci-003",
-            prompt: "Hello, how are you?",
-            max_tokens: 7
+            model: "gpt-4",
+            messages: [{
+                role: "user",
+                content:"Hello!"
+            }]
         })
     })
     const data = await response.json()
     console.log(data)
 }
 
-fetchData()
-
+fetchChatData()
